59dfcdb04efc34fe29999155ef4abebf73e7846f
Armando <hippie.chaka@gmail.com>
2016-06-17 21:12:37 -0500

    ajustes varios

--- sections/about-us.php
+++ sections/about-us.php
@@ -16,8 +16,9 @@
               <div class="info-box info-box5">
                 <div class="img"><i class="fa fa-television"></i></div>
                 <div class="info">
-                  <h2 class="hd">Lorem ipsum dolor</h2>
-                  <p class="sub-txt">Lorem ipsum dolor sit ametconsetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore</p>
+                  <h2 class="hd">Acerca de Nosotros</h2>
+                  <p class="sub-txt">Muchas gracias  por el interés en Jouxbe.  Jouxbe es una compañía pionera en el ramo joyero. Nuestra primer fábrica fue establecida en la ciudad de Guadalajara México en el año  1985. Hasta la fecha sigue siendo administrada por sus fundadores,  desde entonces hemos crecido y ahora tenemos presencia en varios países del continente Americano y Europeo.</p>
+                  <p class="sub-txt">Nosotros en Jouxbe somos especialistas en la fabricación y distribución de joyería en oro laminado. Oro laminado rosa, blanco y amarillo. Tenemos un gran surtido en joyería y joyería personalizada, todo con la más alta calidad.   Lo que es más importante para nosotros es que tu negocio también crezca con productos, calidad y experiencia que aquí te ofrecemos en Jouxbe</p>
                 </div>
               </div><!-- /.info-box -->   
             </div>
@@ -26,31 +27,13 @@
               <div class="info-box info-box5">
                 <div class="img"><i class="fa fa-object-group"></i></div>
                 <div class="info">
-                  <h2 class="hd">Lorem ipsum dolor</h2>
-                  <p class="sub-txt">Lorem ipsum dolor sit ametconsetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore</p>
+                  <h2 class="hd">About Us</h2>
+                  <p class="sub-txt">Thank you for your interest in Jouxbe. We are a pioneer company in the jewelry industry. Our very first factory opened in the city of Guadalajara Mexico in the year 1985. To date, still under management by its founders, we have grown and now have a presence in various countries  in the American and European continents.</p>
+                  <p class="sub-txt">We at Jouxbe are specialists in the manufacturing and distribution of Gold layered, Pink Gold and White gold jewelry. We have a large assortment in jewelry and personalized jewelry, all with the most high quality. what is most important to us is that your business grows with the quality and experience we at Jouxbe offer worldwide. We ship to anywhere across the world. We at Jouxbe offer</p>
                 </div>
               </div><!-- /.info-box -->
             </div>
 
-            <div class="col-sm-6">
-              <div class="info-box info-box5">
-                <div class="img"><i class="fa fa-map-o"></i></div>
-                <div class="info">
-                  <h2 class="hd">Lorem ipsum dolor</h2>
-                  <p class="sub-txt">Lorem ipsum dolor sit ametconsetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore</p>
-                </div>
-              </div><!-- /.info-box -->
-            </div>
-            
-            <div class="col-sm-6">
-              <div class="info-box info-box5">
-                <div class="img"><i class="fa fa-diamond"></i></div>
-                <div class="info">
-                  <h2 class="hd">Lorem ipsum dolor</h2>
-                  <p class="sub-txt">Lorem ipsum dolor sit ametconsetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore</p>
-                </div>
-              </div><!-- /.info-box -->
-            </div>
           </div><!-- /.row -->
         </div><!-- /.content -->
       </div><!-- /.col-lg-6 -->
