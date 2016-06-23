1299738f28a6e468ae9fb91de52b6b99ebfd6e60
Mafiass <churro.mafiass@gmail.com>
2016-06-22 21:38:10 -0500

    rings_9:37

--- sections/products-rings.php
+++ sections/products-rings.php
@@ -27,125 +27,143 @@
 </div>
 
 <div id="links" data-columns class="padding10">
-    <a href="images/products/rings/an-012_gde.jpg" class="item">
-        <img src="images/products/rings/an-012_gde.jpg">
+    <a href="images/products/rings/an-012.jpg" class="item" title="an-012">
+        <img src="images/products/rings/an-012.jpg">
     </a>
-    <a href="images/products/rings/an-013.jpg" class="item">
+    <a href="images/products/rings/an-013.jpg" class="item" title="an-013">
         <img src="images/products/rings/an-013.jpg">
     </a>
-    <a href="images/products/rings/an-014.jpg" class="item">
+    <a href="images/products/rings/an-014.jpg" class="item" title="an-014">
         <img src="images/products/rings/an-014.jpg">
     </a>
-    <a href="images/products/rings/an-015.jpg" class="item">
+    <a href="images/products/rings/an-015.jpg" class="item" title="an-015">
         <img src="images/products/rings/an-015.jpg">
     </a>
-    <a href="images/products/rings/an-016.jpg" class="item">
+    <a href="images/products/rings/an-016.jpg" class="item" title="an-016">
         <img src="images/products/rings/an-016.jpg">
     </a>
-    <a href="images/products/rings/an-017.jpg" class="item">
+    <a href="images/products/rings/an-017.jpg" class="item" title="an-017">
         <img src="images/products/rings/an-017.jpg">
     </a>
-    <a href="images/products/rings/an-018.jpg" class="item">
+    <a href="images/products/rings/an-018.jpg" class="item" title="an-018">
         <img src="images/products/rings/an-018.jpg">
     </a>
-    <a href="images/products/rings/an-019.jpg" class="item">
+    <a href="images/products/rings/an-019.jpg" class="item" title="an-019">
         <img src="images/products/rings/an-019.jpg">
     </a>
-    <a href="images/products/rings/an-020.jpg" class="item">
+    <a href="images/products/rings/an-020.jpg" class="item" title="an-020">
         <img src="images/products/rings/an-020.jpg">
     </a>
-    <a href="images/products/rings/an-021.jpg" class="item">
+    <a href="images/products/rings/an-021.jpg" class="item" title="an-021">
         <img src="images/products/rings/an-021.jpg">
     </a>
-    <a href="images/products/rings/an-022.jpg" class="item">
+    <a href="images/products/rings/an-022.jpg" class="item" title="an-022">
         <img src="images/products/rings/an-022.jpg">
     </a>
-    <a href="images/products/rings/an-024.jpg" class="item">
+    <a href="images/products/rings/an-024.jpg" class="item" title="an-024">
         <img src="images/products/rings/an-024.jpg">
     </a>
-    <a href="images/products/rings/an-023.jpg" class="item">
+    <a href="images/products/rings/an-023.jpg" class="item" title="an-023">
         <img src="images/products/rings/an-023.jpg">
     </a>
-    <a href="images/products/rings/an-025.jpg" class="item">
+    <a href="images/products/rings/an-025.jpg" class="item" title="an-025">
         <img src="images/products/rings/an-025.jpg">
     </a>
-    <a href="images/products/rings/an-026.jpg" class="item">
+    <a href="images/products/rings/an-026.jpg" class="item" title="an-026">
         <img src="images/products/rings/an-026.jpg">
     </a>
-    <a href="images/products/rings/an-027.jpg" class="item">
+    <a href="images/products/rings/an-027.jpg" class="item" title="an-027">
         <img src="images/products/rings/an-027.jpg">
     </a>
-    <a href="images/products/rings/an-028.jpg" class="item">
+    <a href="images/products/rings/an-028.jpg" class="item" title="an-028">
         <img src="images/products/rings/an-028.jpg">
     </a>
-    <a href="images/products/rings/an-029.jpg" class="item">
+    <a href="images/products/rings/an-029.jpg" class="item" title="an-029">
         <img src="images/products/rings/an-029.jpg">
     </a>
-    <a href="images/products/rings/an-030.jpg" class="item">
+    <a href="images/products/rings/an-030.jpg" class="item" title="an-030">
         <img src="images/products/rings/an-030.jpg">
     </a>
-    <a href="images/products/rings/an-031.jpg" class="item">
+    <a href="images/products/rings/an-031.jpg" class="item" title="an-031">
         <img src="images/products/rings/an-031.jpg">
     </a>
-    <a href="images/products/rings/an-032.jpg" class="item">
+    <a href="images/products/rings/an-032.jpg" class="item" title="an-032">
         <img src="images/products/rings/an-032.jpg">
     </a>
-    <a href="images/products/rings/an-033.jpg" class="item">
+    <a href="images/products/rings/an-033.jpg" class="item" title="an-033">
         <img src="images/products/rings/an-033.jpg">
     </a>
-    <a href="images/products/rings/an-034.jpg" class="item">
+    <a href="images/products/rings/an-034.jpg" class="item" title="an-034">
         <img src="images/products/rings/an-034.jpg">
     </a>
-    <a href="images/products/rings/an-035.jpg" class="item">
+    <a href="images/products/rings/an-035.jpg" class="item" title="an-035">
         <img src="images/products/rings/an-035.jpg">
     </a>
-    <a href="images/products/rings/an-036.jpg" class="item">
+    <a href="images/products/rings/an-036.jpg" class="item" title="an-036">
         <img src="images/products/rings/an-036.jpg">
     </a>
-    <a href="images/products/rings/an-037.jpg" class="item">
+    <a href="images/products/rings/an-037.jpg" class="item" title="an-037">
         <img src="images/products/rings/an-037.jpg">
     </a>
-    <a href="images/products/rings/an-038.jpg" class="item">
+    <a href="images/products/rings/an-038.jpg" class="item" title="an-038">
         <img src="images/products/rings/an-038.jpg">
     </a>
-    <a href="images/products/rings/an-039.jpg" class="item">
+    <a href="images/products/rings/an-039.jpg" class="item" title="an-039">
         <img src="images/products/rings/an-039.jpg">
     </a>
-    <a href="images/products/rings/an-040.jpg" class="item">
+    <a href="images/products/rings/an-040.jpg" class="item" title="an-040">
         <img src="images/products/rings/an-040.jpg">
     </a>
-    <a href="images/products/rings/an-041.jpg" class="item">
+    <a href="images/products/rings/an-041.jpg" class="item" title="an-041">
         <img src="images/products/rings/an-041.jpg">
     </a>
-    <a href="images/products/rings/an-042.jpg" class="item">
+    <a href="images/products/rings/an-042.jpg" class="item" title="an-042">
         <img src="images/products/rings/an-042.jpg">
     </a>
-    <a href="images/products/rings/an-043.jpg" class="item">
+    <a href="images/products/rings/an-043.jpg" class="item" title="an-043">
         <img src="images/products/rings/an-043.jpg">
     </a>
-    <a href="images/products/rings/an-044.jpg" class="item">
+    <a href="images/products/rings/an-044.jpg" class="item" title="an-044">
         <img src="images/products/rings/an-044.jpg">
     </a>
-    <a href="images/products/rings/an-045.jpg" class="item">
+    <a href="images/products/rings/an-045.jpg" class="item" title="an-045">
         <img src="images/products/rings/an-045.jpg">
     </a>
-    <a href="images/products/rings/an-046.jpg" class="item">
+    <a href="images/products/rings/an-046.jpg" class="item" title="an-046">
         <img src="images/products/rings/an-046.jpg">
     </a>
-    <a href="images/products/rings/an-047.jpg" class="item">
+    <a href="images/products/rings/an-047.jpg" class="item" title="an-047">
         <img src="images/products/rings/an-047.jpg">
     </a>
-    <a href="images/products/rings/an-048.jpg" class="item">
+    <a href="images/products/rings/an-048.jpg" class="item" title="an-048">
         <img src="images/products/rings/an-048.jpg">
     </a>
-    <a href="images/products/rings/an-049.jpg" class="item">
+    <a href="images/products/rings/an-049.jpg" class="item" title="an-049">
         <img src="images/products/rings/an-049.jpg">
     </a>
-    <a href="images/products/rings/an-050.jpg" class="item">
+    <a href="images/products/rings/an-050.jpg" class="item" title="an-050">
         <img src="images/products/rings/an-050.jpg">
     </a>
-    <a href="images/products/rings/an-051.jpg" class="item">
-        <img src="images/products/rings/an-051.jpg">                        
+    <a href="images/products/rings/an-051.jpg" class="item" title="an-051">
+        <img src="images/products/rings/an-051.jpg">
+    </a>
+    <a href="images/products/rings/an-053.jpg" class="item" title="an-053">
+        <img src="images/products/rings/an-053.jpg">
+    </a>
+    <a href="images/products/rings/an-052.jpg" class="item" title="an-052">
+        <img src="images/products/rings/an-052.jpg">
+    </a>
+    <a href="images/products/rings/an-054.jpg" class="item" title="an-054">
+        <img src="images/products/rings/an-054.jpg">
+    </a>
+    <a href="images/products/rings/an-055.jpg" class="item" title="an-055">
+        <img src="images/products/rings/an-055.jpg">
+    </a>
+    <a href="images/products/rings/an-057.jpg" class="item" title="an-057">
+        <img src="images/products/rings/an-057.jpg">
+    </a>
+    <a href="images/products/rings/an-058.jpg" class="item" title="an-058">
+        <img src="images/products/rings/an-058.jpg">                             
     </a>
 	
 </div>
@@ -154,7 +172,7 @@
 <!-- The Gallery as lightbox dialog, should be a child element of the document body -->
 <div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls">
     <div class="slides"></div>
-    <!-- <h3 class="title"></h3> -->
+    <h3 class="title"></h3>
     <a class="prev">‹</a>
     <a class="next">›</a>
     <a class="close">×</a>
