646a617c6de56ef1e55caae7e41327cc953f3c1a
Mafiass <churro.mafiass@gmail.com>
2016-06-27 19:10:16 -0500

    lunes de lluvia, 7:08, 27-06-16

--- sections/products-charms.php
+++ sections/products-charms.php
@@ -27,50 +27,161 @@
 </div>
 
 <div id="links" data-columns class="padding10">
-    <a href="http://placehold.it/1024x800" class="item">
-        <img src="http://placehold.it/600x842">
+    <a href="images/products/pendants/dj-001.jpg" class="item" title="dj-001">
+        <img src="images/products/pendants/dj-001.jpg">
     </a>
-    <a href="http://placehold.it/1024x800" class="item">
-        <img src="http://placehold.it/600x390">
+    <a href="images/products/pendants/dj-002.jpg" class="item" title="dj-002">
+        <img src="images/products/pendants/dj-002.jpg">
     </a>
-    <a href="http://placehold.it/1024x800" class="item">
-        <img src="http://placehold.it/600x408">
+    <a href="images/products/pendants/dj-003.jpg" class="item" title="dj-003">
+        <img src="images/products/pendants/dj-003.jpg">
     </a>
-    <a href="http://placehold.it/1024x800" class="item">
-        <img src="http://placehold.it/600x221">
+    <a href="images/products/pendants/dj-004.jpg" class="item" title="dj-004">
+        <img src="images/products/pendants/dj-004.jpg">
     </a>
-    <a href="http://placehold.it/1024x800" class="item">
-        <img src="http://placehold.it/600x450">
+    <a href="images/products/pendants/dj-005.jpg" class="item" title="dj-005">
+        <img src="images/products/pendants/dj-005.jpg">
     </a>
-    <a href="http://placehold.it/1024x800" class="item">
-        <img src="http://placehold.it/600x350">
+    <a href="images/products/pendants/dj-006.jpg" class="item" title="dj-006">
+        <img src="images/products/pendants/dj-006.jpg">
     </a>
-    <a href="http://placehold.it/1024x800" class="item">
-        <img src="http://placehold.it/600x253">
+    <a href="images/products/pendants/dj-007.jpg" class="item" title="dj-007">
+        <img src="images/products/pendants/dj-007.jpg">
     </a>
-    <a href="http://placehold.it/1024x800" class="item">
-        <img src="http://placehold.it/600x600">
+    <a href="images/products/pendants/dj-008.jpg" class="item" title="dj-008">
+        <img src="images/products/pendants/dj-008.jpg">
     </a>
-    <a href="http://placehold.it/1024x800" class="item">
-        <img src="http://placehold.it/600x390">
+    <a href="images/products/pendants/dj-009.jpg" class="item" title="dj-009">
+        <img src="images/products/pendants/dj-009.jpg">
     </a>
-    <a href="http://placehold.it/1024x800" class="item">
-        <img src="http://placehold.it/600x260">
+    <a href="images/products/pendants/dj-010.jpg" class="item" title="dj-010">
+        <img src="images/products/pendants/dj-010.jpg">
     </a>
-    <a href="http://placehold.it/1024x800" class="item">
-        <img src="http://placehold.it/600x400">
+    <a href="images/products/pendants/dj-011.jpg" class="item" title="dj-011">
+        <img src="images/products/pendants/dj-011.jpg">
     </a>
-    <a href="http://placehold.it/1024x800" class="item">
-        <img src="http://placehold.it/600x550">
+    <a href="images/products/pendants/dj-071.jpg" class="item" title="dj-071">
+        <img src="images/products/pendants/dj-071.jpg">
     </a>
-    <a href="http://placehold.it/1024x800" class="item">
-        <img src="http://placehold.it/600x400">
+    <a href="images/products/pendants/dj-073.jpg" class="item" title="dj-073">
+        <img src="images/products/pendants/dj-073.jpg">
     </a>
-    <a href="http://placehold.it/1024x800" class="item">
-        <img src="http://placehold.it/600x360">
+    <a href="images/products/pendants/dj-074.jpg" class="item" title="dj-074">
+        <img src="images/products/pendants/dj-074.jpg">
     </a>
-    <a href="http://placehold.it/1024x800" class="item">
-        <img src="http://placehold.it/600x390">
+    <a href="images/products/pendants/dj-075.jpg" class="item" title="dj-075">
+        <img src="images/products/pendants/dj-075.jpg">
+    </a>
+    <a href="images/products/pendants/dj-076.jpg" class="item" title="dj-076">
+        <img src="images/products/pendants/dj-076.jpg">
+    </a>
+    <a href="images/products/pendants/dj-077.jpg" class="item" title="dj-077">
+        <img src="images/products/pendants/dj-077.jpg">
+    </a>
+    <a href="images/products/pendants/dj-078.jpg" class="item" title="dj-078">
+        <img src="images/products/pendants/dj-078.jpg">
+    </a>
+    <a href="images/products/pendants/dj-079.jpg" class="item" title="dj-079">
+        <img src="images/products/pendants/dj-079.jpg">
+    </a>
+    <a href="images/products/pendants/dj-080.jpg" class="item" title="dj-080">
+        <img src="images/products/pendants/dj-080.jpg">
+        </a>
+    <a href="images/products/pendants/dj-125.jpg" class="item" title="dj-125">
+        <img src="images/products/pendants/dj-125.jpg">
+    </a>
+    <a href="images/products/pendants/dj-129.jpg" class="item" title="dj-129">
+        <img src="images/products/pendants/dj-129.jpg">
+    </a>
+    <a href="images/products/pendants/dj-130.jpg" class="item" title="dj-130">
+        <img src="images/products/pendants/dj-130.jpg">
+    </a>
+    <a href="images/products/pendants/dj-132.jpg" class="item" title="dj-132">
+        <img src="images/products/pendants/dj-132.jpg">
+    </a>
+    <a href="images/products/pendants/dj-133.jpg" class="item" title="dj-133">
+        <img src="images/products/pendants/dj-133.jpg">
+    </a>
+    <a href="images/products/pendants/dj-134.jpg" class="item" title="dj-134">
+        <img src="images/products/pendants/dj-134.jpg">
+    </a>
+    <a href="images/products/pendants/dj-135.jpg" class="item" title="dj-135">
+        <img src="images/products/pendants/dj-135.jpg">
+    </a>
+    <a href="images/products/pendants/dj-136.jpg" class="item" title="dj-136">
+        <img src="images/products/pendants/dj-136.jpg">
+    </a>
+    <a href="images/products/pendants/dj-137.jpg" class="item" title="dj-137">
+        <img src="images/products/pendants/dj-137.jpg">
+    </a>
+    <a href="images/products/pendants/dj-138.jpg" class="item" title="dj-138">
+        <img src="images/products/pendants/dj-138.jpg">
+    </a>
+    <a href="images/products/pendants/dj-139.jpg" class="item" title="dj-139">
+        <img src="images/products/pendants/dj-139.jpg">
+    </a>
+    <a href="images/products/pendants/dj-140.jpg" class="item" title="dj-140">
+        <img src="images/products/pendants/dj-140.jpg">
+    </a>
+    <a href="images/products/pendants/dj-141.jpg" class="item" title="dj-141">
+        <img src="images/products/pendants/dj-141.jpg">
+        </a>
+    <a href="images/products/pendants/dj-142.jpg" class="item" title="dj-142">
+        <img src="images/products/pendants/dj-142.jpg">
+    </a>
+    <a href="images/products/pendants/dj-143.jpg" class="item" title="dj-143">
+        <img src="images/products/pendants/dj-143.jpg">
+    </a>
+    <a href="images/products/pendants/dj-144.jpg" class="item" title="dj-144">
+        <img src="images/products/pendants/dj-144.jpg">
+    </a>
+    <a href="images/products/pendants/dj-145.jpg" class="item" title="dj-145">
+        <img src="images/products/pendants/dj-145.jpg">
+    </a>
+    <a href="images/products/pendants/dj-146.jpg" class="item" title="dj-146">
+        <img src="images/products/pendants/dj-146.jpg">
+    </a>
+    <a href="images/products/pendants/dj-147.jpg" class="item" title="dj-147">
+        <img src="images/products/pendants/dj-147.jpg">
+    </a>
+    <a href="images/products/pendants/dj-148.jpg" class="item" title="dj-148">
+        <img src="images/products/pendants/dj-148.jpg">
+    </a>
+    <a href="images/products/pendants/dj-149.jpg" class="item" title="dj-149">
+        <img src="images/products/pendants/dj-149.jpg">
+    </a>
+    <a href="images/products/pendants/dj-150.jpg" class="item" title="dj-150">
+        <img src="images/products/pendants/dj-150.jpg">
+    </a>
+    <a href="images/products/pendants/dj-155.jpg" class="item" title="dj-155">
+        <img src="images/products/pendants/dj-155.jpg">
+    </a>
+    <a href="images/products/pendants/dj-156.jpg" class="item" title="dj-156">
+        <img src="images/products/pendants/dj-156.jpg">
+    </a>
+    <a href="images/products/pendants/dj-157.jpg" class="item" title="dj-157">
+        <img src="images/products/pendants/dj-157.jpg">
+    </a>
+    <a href="images/products/pendants/dj-158.jpg" class="item" title="dj-158">
+        <img src="images/products/pendants/dj-158.jpg">
+    </a>
+    <a href="images/products/pendants/dj-159.jpg" class="item" title="dj-159">
+        <img src="images/products/pendants/dj-159.jpg">
+    </a>
+    <a href="images/products/pendants/dj-160.jpg" class="item" title="dj-160">
+        <img src="images/products/pendants/dj-160.jpg">
+    </a>
+    <a href="images/products/pendants/dj-161.jpg" class="item" title="dj-161">
+        <img src="images/products/pendants/dj-161.jpg">
+    </a>
+    <a href="images/products/pendants/dj-163.jpg" class="item" title="dj-163">
+        <img src="images/products/pendants/dj-163.jpg">
+    </a>
+    <a href="images/products/pendants/dj-164.jpg" class="item" title="dj-164">
+        <img src="images/products/pendants/dj-164.jpg">
+    </a>
+    <a href="images/products/pendants/dj-165.jpg" class="item" title="dj-165">
+        <img src="images/products/pendants/dj-165.jpg">
     </a>
 	
 </div>
@@ -79,7 +190,7 @@
 <!-- The Gallery as lightbox dialog, should be a child element of the document body -->
 <div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls">
     <div class="slides"></div>
-    <!-- <h3 class="title"></h3> -->
+    <h3 class="title"></h3>
     <a class="prev">‹</a>
     <a class="next">›</a>
     <a class="close">×</a>
--- sections/products.php
+++ sections/products.php
@@ -27,50 +27,110 @@
 </div>
 
 <div id="links" data-columns class="padding10">
-    <a href="images/products/bangles/image1.jpg" class="item">
-        <img src="images/products/bangles/image1_thumb.jpg">
+    <a href="images/products/bangles/se-001.jpg" class="item" title="se-001">
+        <img src="images/products/bangles/se-001.jpg">
     </a>
-    <a href="images/products/bangles/image1.jpg" class="item">
-        <img src="images/products/bangles/image1_thumb.jpg">
+    <a href="images/products/bangles/se-002.jpg" class="item" title="se-002">
+        <img src="images/products/bangles/se-002.jpg">
     </a>
-    <a href="images/products/bangles/image1.jpg" class="item">
-        <img src="images/products/bangles/image1_thumb.jpg">
+    <a href="images/products/bangles/se-003.jpg" class="item" title="se-003">
+        <img src="images/products/bangles/se-003.jpg">
     </a>
-    <a href="images/products/bangles/image1.jpg" class="item">
-        <img src="images/products/bangles/image1_thumb.jpg">
+    <a href="images/products/bangles/se-004.jpg" class="item" title="se-004">
+        <img src="images/products/bangles/se-004.jpg">
     </a>
-    <a href="images/products/bangles/image1.jpg" class="item">
-        <img src="images/products/bangles/image1_thumb.jpg">
+    <a href="images/products/bangles/se-005.jpg" class="item" title="se-005">
+        <img src="images/products/bangles/se-005.jpg">
     </a>
-    <a href="images/products/bangles/image1.jpg" class="item">
-        <img src="images/products/bangles/image1_thumb.jpg">
+    <a href="images/products/bangles/se-006.jpg" class="item" title="se-006">
+        <img src="images/products/bangles/se-006.jpg">
     </a>
-    <a href="images/products/bangles/image1.jpg" class="item">
-        <img src="images/products/bangles/image1_thumb.jpg">
+    <a href="images/products/bangles/se-007.jpg" class="item" title="se-007">
+        <img src="images/products/bangles/se-007.jpg">
     </a>
-    <a href="images/products/bangles/image1.jpg" class="item">
-        <img src="images/products/bangles/image1_thumb.jpg">
+    <a href="images/products/bangles/se-008.jpg" class="item" title="se-008">
+        <img src="images/products/bangles/se-008.jpg">
     </a>
-    <a href="images/products/bangles/image1.jpg" class="item">
-        <img src="images/products/bangles/image1_thumb.jpg">
+    <a href="images/products/bangles/se-009.jpg" class="item" title="se-009">
+        <img src="images/products/bangles/se-009.jpg">
     </a>
-    <a href="images/products/bangles/image1.jpg" class="item">
-        <img src="images/products/bangles/image1_thumb.jpg">
+    <a href="images/products/bangles/se-010.jpg" class="item" title="se-010">
+        <img src="images/products/bangles/se-010.jpg">
     </a>
-    <a href="images/products/bangles/image1.jpg" class="item">
-        <img src="images/products/bangles/image1_thumb.jpg">
+    <a href="images/products/bangles/se-011.jpg" class="item" title="se-011">
+        <img src="images/products/bangles/se-011.jpg">
     </a>
-    <a href="images/products/bangles/image1.jpg" class="item">
-        <img src="images/products/bangles/image1_thumb.jpg">
+    <a href="images/products/bangles/se-012.jpg" class="item" title="se-012">
+        <img src="images/products/bangles/se-012.jpg">
     </a>
-    <a href="images/products/bangles/image1.jpg" class="item">
-        <img src="images/products/bangles/image1_thumb.jpg">
+    <a href="images/products/bangles/se-013.jpg" class="item" title="se-013">
+        <img src="images/products/bangles/se-013.jpg">
     </a>
-    <a href="images/products/bangles/image1.jpg" class="item">
-        <img src="images/products/bangles/image1_thumb.jpg">
+    <a href="images/products/bangles/se-014.jpg" class="item" title="se-014">
+        <img src="images/products/bangles/se-014.jpg">
     </a>
-    <a href="images/products/bangles/image1.jpg" class="item">
-        <img src="images/products/bangles/image1_thumb.jpg">
+    <a href="images/products/bangles/se-015.jpg" class="item" title="se-015">
+        <img src="images/products/bangles/se-015.jpg">
+    </a>
+    <a href="images/products/bangles/se-016.jpg" class="item" title="se-016">
+        <img src="images/products/bangles/se-016.jpg">
+    </a>
+    <a href="images/products/bangles/se-017.jpg" class="item" title="se-017">
+        <img src="images/products/bangles/se-017.jpg">
+    </a>
+    <a href="images/products/bangles/se-018.jpg" class="item" title="se-018">
+        <img src="images/products/bangles/se-018.jpg">
+    </a>
+    <a href="images/products/bangles/se-019.jpg" class="item" title="se-019">
+        <img src="images/products/bangles/se-019.jpg">
+    </a>
+    <a href="images/products/bangles/se-020.jpg" class="item" title="se-020">
+        <img src="images/products/bangles/se-020.jpg">
+    </a>
+    <a href="images/products/bangles/se-021.jpg" class="item" title="se-021">
+        <img src="images/products/bangles/se-021.jpg">
+    </a>
+    <a href="images/products/bangles/se-022.jpg" class="item" title="se-022">
+        <img src="images/products/bangles/se-022.jpg">
+    </a>
+    <a href="images/products/bangles/se-023.jpg" class="item" title="se-023">
+        <img src="images/products/bangles/se-023.jpg">
+    </a>
+    <a href="images/products/bangles/se-025.jpg" class="item" title="se-025">
+        <img src="images/products/bangles/se-025.jpg">
+    </a>
+    <a href="images/products/bangles/se-026.jpg" class="item" title="se-026">
+        <img src="images/products/bangles/se-026.jpg">
+    </a>
+    <a href="images/products/bangles/se-027.jpg" class="item" title="se-027">
+        <img src="images/products/bangles/se-027.jpg">
+    </a>
+    <a href="images/products/bangles/se-028.jpg" class="item" title="se-028">
+        <img src="images/products/bangles/se-028.jpg">
+    </a>
+    <a href="images/products/bangles/se-029.jpg" class="item" title="se-029">
+        <img src="images/products/bangles/se-029.jpg">
+    </a>
+    <a href="images/products/bangles/se-030.jpg" class="item" title="se-030">
+        <img src="images/products/bangles/se-030.jpg">
+    </a>
+    <a href="images/products/bangles/se-031.jpg" class="item" title="se-031">
+        <img src="images/products/bangles/se-031.jpg">
+    </a>
+    <a href="images/products/bangles/se-032.jpg" class="item" title="se-032">
+        <img src="images/products/bangles/se-032.jpg">
+    </a>
+    <a href="images/products/bangles/se-033.jpg" class="item" title="se-033">
+        <img src="images/products/bangles/se-033.jpg">
+    </a>
+    <a href="images/products/bangles/se-034.jpg" class="item" title="se-034">
+        <img src="images/products/bangles/se-034.jpg">
+    </a>
+    <a href="images/products/bangles/se-035.jpg" class="item" title="se-035">
+        <img src="images/products/bangles/se-035.jpg">
+    </a>
+    <a href="images/products/bangles/se-036.jpg" class="item" title="se-036">
+        <img src="images/products/bangles/se-036.jpg">
     </a>
 	
 </div>
@@ -79,7 +139,7 @@
 <!-- The Gallery as lightbox dialog, should be a child element of the document body -->
 <div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls">
     <div class="slides"></div>
-    <!-- <h3 class="title"></h3> -->
+    <h3 class="title"></h3>
     <a class="prev">‹</a>
     <a class="next">›</a>
     <a class="close">×</a>
